f85da3d9f8c3532afd079a283c39b62f97bd1d43
lotfi-al <lotfi.amin@hotmail.com>
2021-02-23 20:19:53 +0100

    Cleaned up the demo. Made a MVP of the homepage.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,20 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:bro/screens/flutter-demo/demoscreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bro/main.dart';
+Widget createWidgetForTesting({Widget child}){
+  return MaterialApp(
+    home: child,
+  );
+}
 
 void main() {
   testWidgets('Counter increments smoke test', (tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(createWidgetForTesting(child: MyHomePage(title:'heylo')));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
