d5896822e4286560a4a575fa4337555f93614963
Patrick Moen Allport <pa10@pop-os.localdomain>
2021-03-26 19:49:51 +0100

    Fixed Tests to match mocktail

--- test/blocs/category_bloc_test.dart
+++ test/blocs/category_bloc_test.dart
@@ -5,13 +5,22 @@ import 'package:bro/data/category_repository.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 
 import '../mock_data/category_mock.dart';
 
 class MockCategoryRepository extends Mock implements CategoryRepository {}
 
-void main() async {
+void main() {
+  setUpAll(() {
+    registerFallbackValue<CategoryState>(Failed());
+    registerFallbackValue<CategoryEvent>(CategoriesRequested());
+  });
+
+  mainTest();
+}
+
+void mainTest() async {
   // For DotEnv
   TestWidgetsFlutterBinding.ensureInitialized();
   await DotEnv.load();
@@ -22,7 +31,7 @@ void main() async {
 
     setUp(() {
       categoryRepository = MockCategoryRepository();
-      when(categoryRepository.getCategories()).thenAnswer(
+      when(() => categoryRepository.getCategories()).thenAnswer(
           (_) => Future.value(QueryResult(source: null, data: mockedResult)));
       categoryBloc = CategoryBloc(repository: categoryRepository);
     });
@@ -30,24 +39,25 @@ void main() async {
     blocTest(
       'should emit Failed if repository throws',
       build: () {
-        when(categoryRepository.getCategories()).thenThrow(Exception('Woops'));
+        when(() => categoryRepository.getCategories())
+            .thenThrow(Exception('Woops'));
         return categoryBloc;
       },
       act: (CategoryBloc bloc) async => bloc.add(CategoriesRequested()),
-      expect: <CategoryState>[Failed()],
+      expect: () => <CategoryState>[Failed()],
     );
 
     blocTest(
       'Initial state is correct',
       build: () => categoryBloc,
-      expect: <CategoryState>[],
+      expect: () => <CategoryState>[],
     );
 
     blocTest(
       'should load items in response to a CategoriesRequested event',
       build: () => categoryBloc,
       act: (CategoryBloc bloc) async => bloc.add(CategoriesRequested()),
-      expect: [
+      expect: () => [
         isA<Success>(),
       ],
     );
