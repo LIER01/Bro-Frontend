31e22a21ee3ae4affcf040b7c1fddc4cc04f3448
Patrick Moen Allport <pa10@pop-os.localdomain>
2021-04-19 13:48:37 +0200

    Fixed bottomloader showing up when not loading and added padding

--- lib/views/widgets/bottom_loader.dart
+++ lib/views/widgets/bottom_loader.dart
@@ -4,6 +4,9 @@ class BottomLoader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.02,
+          vertical: MediaQuery.of(context).size.height * 0.02),
       alignment: Alignment.center,
       child: Center(
         child: SizedBox(
